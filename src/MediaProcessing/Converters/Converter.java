119187d6d7ecbcf71121909c3e026c351fff180d
PhilipPragerUrbina <72355251+PhilipPragerUrbina@users.noreply.github.com>
2023-01-15 15:35:24 -0800

    Added more filters

---
+++ src/MediaProcessing/Converters/Converter.java
@@ -0,0 +1,17 @@
+package MediaProcessing.Converters;
+
+import MediaProcessing.Data.Image;
+import MediaProcessing.Utils.Color;
+
+/**
+ * Convert between image types
+ */
+public interface Converter <InType extends Color, OutType extends Color>{
+
+    /**
+     * Convert between two color types
+     * @param input Input image
+     * @return New image with converted data
+     */
+    Image<OutType> convert(Image<InType> input);
+}
--- src/MediaProcessing/Filters/BorderFilter.java
+++ src/MediaProcessing/Filters/BorderFilter.java
@@ -8,17 +8,32 @@ import MediaProcessing.Utils.Colors.RGBA;
  */
 public class BorderFilter implements Filter<RGBA> {
     private RGBA border_color;
+    private int border_width;
 
     /**
      * Add a "picture frame" like border
      * @param border_color Color of border
+     * @param border_width Border width in pixels
      */
-    public BorderFilter(RGBA border_color){
+    public BorderFilter(RGBA border_color, int border_width){
         this.border_color = border_color;
+        this.border_width = border_width;
     }
 
     @Override
     public void apply(Image<RGBA> image) {
-        //todo downscale image
+        Image<RGBA> temporary_image = new Image<>(image.getWidth() + 2*border_width,image.getHeight() + 2*border_width); //Create resized image
+
+        for (int x = 0; x < temporary_image.getWidth(); x++) { //Iterate over image
+            for (int y = 0; y < temporary_image.getHeight(); y++) {
+                if(x >= border_width && x < temporary_image.getWidth()-border_width &&
+                        y >= border_width && y < temporary_image.getHeight()-border_width){ //If within original image area
+                        temporary_image.setPixel(x,y,image.getPixel(x - border_width,y-border_width)); //Copy over original image data
+                }else{
+                    temporary_image.setPixel(x,y,border_color); //Set to border color
+                }
+            }
+        }
+        image.setData(temporary_image); //Set original image to new image
     }
 }
